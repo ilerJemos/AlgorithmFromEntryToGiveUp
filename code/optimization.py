08f4c5cace0ad8f8575a0c3595fce785e36232e5
ilerJemos <13126795011@163.com>
2018-08-19 13:49:20 +0800

    add dp from the bottom

--- code/optimization.py
+++ code/optimization.py
@@ -8,6 +8,7 @@ def fib(n):
     if n<=1:return 1
     else: return fib(n-1)+fib(n-2)
 #n
+#自顶而下
 def fastfib(n,memo):
     if not n in memo:
         memo[n] = fastfib(n-1,memo)+fastfib(n-2,memo)
@@ -15,3 +16,22 @@ def fastfib(n,memo):
 def fib1(n):
     memo = {0:1,1:1}
     return fastfib(n,memo)
+#自底而上
+def fib2(n):
+    if n<= 1:
+        return 1
+    memo =[1,1]
+    for i in range(2,n+1):
+        temp=memo[i-1]+memo[i-2]
+        memo.append(temp)
+    return memo[n]
+#压缩空间
+def fib22(n):
+    if n<= 1:
+        return 1
+    memo_0=memo_1 = 1
+    for i in range(2,n+1):
+        memo = memo_0+memo_1
+        memo_1 = memo_0
+        memo_0 = memo
+    return memo
---
+++ smallProj/3DrunkBoy.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 17 19:49:42 2018
+
+@author: 13548
+"""
+
+#biaed random walk
