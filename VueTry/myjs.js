b6b5e312f2a0ce7109ad765ae6ad56209e4c27c9
ilerJemos <13126795011@163.com>
2018-10-26 18:15:36 +0800

    html and javascript

---
+++ VueTry/myjs.js
@@ -0,0 +1,7 @@
+//三种输出方式
+function outputTest(){
+    window.alert("window alert")
+    console.log("console log")
+    document.getElementById("demo").innerHTML="outputtest"
+    //document.write("donot test!")
+}
---
+++ VueTry/vue.js
@@ -0,0 +1,8 @@
+<script src="https://cdn.staticfile.org/vue/2.4.2/vue.min.js"></script>
+
+var app = new Vue({
+    el:'#app',
+    data:{
+        message:'hello'
+    }
+})
